089cb71b687f7dd4a2cc987fcebab6f5ec7514ab
Kaming <jiamingwu233@gmail.com>
2018-07-12 14:15:20 +0800

    Add new question.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # LeetCode
+
+#### Tips: Question description and examples in the code.
+
+### [Easy](src/easy)
+- [Two Sum(两数之和)](src/easy/TwoSum.java)
+
+### [Medium](src/medium)
+
+### [Hard](src/hard)
--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -1,7 +1,10 @@
 package easy;
 
 /**
- * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数的数组下标。
  * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
  *
  * Created by Kaming on 2018/7/12.
@@ -9,12 +12,12 @@ package easy;
 public class TwoSum {
 
     /**
-     * 示例:
+     * Example:
      *
-     * 给定 nums = [2, 7, 11, 15], target = 9
+     * Given nums = [2, 7, 11, 15], target = 9
      *
-     * 因为 nums[0] + nums[1] = 2 + 7 = 9
-     * 所以返回 [0, 1]
+     * Because nums[0] + nums[1] = 2 + 7 = 9
+     * return [0, 1]
      */
     public static void main(String[] args){
        int[] sum = twoSum(new int[]{2,7,11,15},9);
